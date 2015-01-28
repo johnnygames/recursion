eef062d1101724abcea537fc931db48aa05def8e
Johnnygames <johnegames@gmail.com>
2015-01-27 22:59:50 -0800

    This is still not working, I can't quite figure out how to work it with different nodes so I think I'm just going to try it with document.body and go from there

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,6 +8,25 @@ var getElementsByClassName = function(className){
   //establish an empty array to hold the results
   var results;
 
+  function travelDOM (initial_node) {
+  	if (!initial_node) {
+  		initial_node = document.body;
+	}
+	for (var i = 0, counter = initial_node.childNodes.length; i < counter; i++) {
+		if (initial_node.childNodes[i].childNodes.length > 0) {
+			travelDOM(initial_node.childNodes[i]);
+		}
+
+		if (initial_node.childNodes[i].className && initial_node.className.indexOf(className) >= 0) {
+			results.push(initial_node.childNodes[i]);
+		}
+	}
+
+  }
+
+  travelDOM(document.body);
+  return results;
+
   /** The first thing that we need to do is write an inner function
   
   function(param) 
@@ -23,6 +42,7 @@ var getElementsByClassName = function(className){
   	if the className && the indexOf(className) >= 0 exists
   		push it into the results
 
-  return results
-  	**/
+  return resultsg
+  
+  **/
 };
